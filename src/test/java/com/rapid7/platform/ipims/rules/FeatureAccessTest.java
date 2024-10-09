907e24fa50bbf7a9106e5648b7747634e8972580
John Reid <jreid@rapid7.com>
2024-10-09 08:54:07 +0100

    UAP Data Type experimentation

--- src/test/java/com/rapid7/platform/ipims/rules/FeatureAccessTest.java
+++ src/test/java/com/rapid7/platform/ipims/rules/FeatureAccessTest.java
@@ -1,89 +1,91 @@
-package com.rapid7.platform.ipims.rules;
+// package com.rapid7.platform.ipims.rules;
 
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.is;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+// import static org.hamcrest.Matchers.hasItem;
+// import static org.hamcrest.Matchers.hasItems;
+// import static org.hamcrest.Matchers.is;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.web.server.LocalServerPort;
+// import org.springframework.test.annotation.DirtiesContext;
 
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-import io.restassured.http.ContentType;
+// import io.restassured.RestAssured;
+// import static io.restassured.RestAssured.given;
+// import io.restassured.http.ContentType;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
-public class FeatureAccessTest {
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
+// @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
+// public class FeatureAccessTest {
 
-    @LocalServerPort
-    private int port;
+//     @LocalServerPort
+//     private int port;
 
-    @Test
-    public void testEvaluateTrafficViolation() {
-        RestAssured.port = port;
-        given()
-                .body("""
-                    {
-                        "Org Products": [
-                            {
-                              "Organization Id": "org1",
-                              "Product Codes": [
-                                "ICS", "OPS", "IVM"
-                              ]
-                            },
-                            {
-                              "Organization Id": "org2",
-                              "Product Codes": [
-                                "ICS", "OPS"
-                              ]
-                            },
-                            {
-                              "Organization Id": "org3",
-                              "Product Codes": [
-                                "ICS", "IVM"
-                              ]
-                            }
-                          ],
-                          "Org Feature Permissions": [
-                            {
-                              "Organization Id": "org1",
-                              "Feature Permissions": [
-                                  {
-                                    "Feature": "IVM_EXECUTIVE_VIEW",
-                                    "Permission": "ADMINISTER"
-                                  },
-                                  {
-                                    "Feature": "ICS_EXECUTIVE_VIEW",
-                                    "Permission": "ADMINISTER"
-                                  }
-                              ]
-                            },
-                            {
-                              "Organization Id": "org2",
-                              "Feature Permissions": [
-                                  {
-                                    "Feature": "IVM_EXECUTIVE_VIEW",
-                                    "Permission": "ADMINISTER"
-                                  }
-                              ]
-                            },
-                            {
-                              "Organization Id": "org3",
-                              "Feature Permissions": [
-                                  {
-                                    "Feature": "ICS_EXECUTIVE_VIEW",
-                                    "Permission": "ADMINISTER"
-                                  }
-                              ]
-                            }
-                          ]
-                    }""")
-                .contentType(ContentType.JSON)
-                .when()
-                .post("/FeatureAccess")
-                .then()
-                .statusCode(200)
-                .body("'Org with required products'", hasItems("org1", "org3"));
-    }
-}
+//     @Test
+//     public void testEvaluateTrafficViolation() {
+//         RestAssured.port = port;
+//         given()
+//                 .body("""
+//                     {
+//                         "Org Products": [
+//                             {
+//                               "Organization Id": "org1",
+//                               "Product Codes": [
+//                                 "ICS", "OPS", "IVM"
+//                               ]
+//                             },
+//                             {
+//                               "Organization Id": "org2",
+//                               "Product Codes": [
+//                                 "ICS", "OPS"
+//                               ]
+//                             },
+//                             {
+//                               "Organization Id": "org3",
+//                               "Product Codes": [
+//                                 "ICS", "IVM"
+//                               ]
+//                             }
+//                           ],
+//                           "Org Feature Permissions": [
+//                             {
+//                               "Organization Id": "org1",
+//                               "Feature Permissions": [
+//                                   {
+//                                     "Feature": "IVM_EXECUTIVE_VIEW",
+//                                     "Permission": "ADMINISTER"
+//                                   },
+//                                   {
+//                                     "Feature": "ICS_EXECUTIVE_VIEW",
+//                                     "Permission": "ADMINISTER"
+//                                   }
+//                               ]
+//                             },
+//                             {
+//                               "Organization Id": "org2",
+//                               "Feature Permissions": [
+//                                   {
+//                                     "Feature": "IVM_EXECUTIVE_VIEW",
+//                                     "Permission": "ADMINISTER"
+//                                   }
+//                               ]
+//                             },
+//                             {
+//                               "Organization Id": "org3",
+//                               "Feature Permissions": [
+//                                   {
+//                                     "Feature": "ICS_EXECUTIVE_VIEW",
+//                                     "Permission": "ADMINISTER"
+//                                   }
+//                               ]
+//                             }
+//                           ]
+//                     }""")
+//                 .contentType(ContentType.JSON)
+//                 .when()
+//                 .post("/FeatureAccess")
+//                 .then()
+//                 .statusCode(200)
+//                 .body("'Orgs with required products'", hasItems("org1", "org3"))
+//                 .body("'Orgs with required feature permissions'", is("org1"))
+//                 .body("'ERV'", hasItems("org1"));
+//     }
+// }
--- src/test/java/com/rapid7/platform/ipims/rules/TrafficViolationTest.java
+++ src/test/java/com/rapid7/platform/ipims/rules/TrafficViolationTest.java
@@ -1,57 +1,57 @@
-/*
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.rapid7.platform.ipims.rules;
+// /*
+//  * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *       http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+// package com.rapid7.platform.ipims.rules;
 
-import static org.hamcrest.Matchers.is;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+// import static org.hamcrest.Matchers.is;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.web.server.LocalServerPort;
+// import org.springframework.test.annotation.DirtiesContext;
 
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-import io.restassured.http.ContentType;
+// import io.restassured.RestAssured;
+// import static io.restassured.RestAssured.given;
+// import io.restassured.http.ContentType;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
-public class TrafficViolationTest {
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
+// @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
+// public class TrafficViolationTest {
 
-    @LocalServerPort
-    private int port;
+//     @LocalServerPort
+//     private int port;
 
-    @Test
-    public void testEvaluateTrafficViolation() {
-        RestAssured.port = port;
-        given()
-                .body("""
-                      {
-                          "Driver": {
-                              "Points": 2
-                          },
-                          "Violation": {
-                              "Type": "speed",
-                              "Actual Speed": 120,
-                              "Speed Limit": 100
-                          }
-                      }""")
-                .contentType(ContentType.JSON)
-                .when()
-                .post("/TrafficViolation")
-                .then()
-                .statusCode(200)
-                .body("'Should the driver be suspended?'", is("No"));
-    }
-}
+//     @Test
+//     public void testEvaluateTrafficViolation() {
+//         RestAssured.port = port;
+//         given()
+//                 .body("""
+//                       {
+//                           "Driver": {
+//                               "Points": 2
+//                           },
+//                           "Violation": {
+//                               "Type": "speed",
+//                               "Actual Speed": 120,
+//                               "Speed Limit": 100
+//                           }
+//                       }""")
+//                 .contentType(ContentType.JSON)
+//                 .when()
+//                 .post("/TrafficViolation")
+//                 .then()
+//                 .statusCode(200)
+//                 .body("'Should the driver be suspended?'", is("No"));
+//     }
+// }
